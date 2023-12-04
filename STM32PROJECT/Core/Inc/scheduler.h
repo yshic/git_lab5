5e1a8746a882ace15cdc7787e1c7643f92195c44
yshic <tuanl799@gmail.com>
2023-12-04 18:04:57 +0700

    change scheduler header, delete unused variables and functions

--- STM32PROJECT/Core/Inc/scheduler.h
+++ STM32PROJECT/Core/Inc/scheduler.h
@@ -1,10 +1,3 @@
-/*
- * app_scheduler.h
- *
- *  Created on: Nov 27, 2019
- *      Author: VAIO
- */
-
 #ifndef APP_SCHEDULER_H_
 #define APP_SCHEDULER_H_
 
@@ -12,19 +5,11 @@
 
 
 #define SCH_MAX_TASKS 			10
-#define	NO_TASK_ID				0
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+void SCH_Init(void);
+void SCH_Update(void);
+uint32_t SCH_Add_Task(void (*p_function)(), uint32_t DELAY, uint32_t PERIOD);
+void SCH_Dispatch_Tasks(void);
+uint8_t SCH_Delete_Task(uint32_t TASK_ID);
 
-    void SCH_Init(void);
-    void SCH_Update(void);
-    uint32_t SCH_Add_Task(void (*p_function)(), uint32_t DELAY, uint32_t PERIOD);
-    void SCH_Dispatch_Tasks(void);
-    uint8_t SCH_Delete_Task(uint32_t TASK_ID);
-
-#ifdef __cplusplus
-}   
-#endif
 #endif /* APP_SCHEDULER_H_ */
