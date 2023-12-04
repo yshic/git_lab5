bf855a5ab34b2d91b22ea91a518ae226755fd664
yshic <tuanl799@gmail.com>
2023-12-04 18:51:45 +0700

    final

--- STM32PROJECT/Debug/Core/Src/main.d
+++ STM32PROJECT/Debug/Core/Src/main.d
@@ -26,9 +26,8 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_uart.h \
- ../Core/Inc/scheduler.h ../Core/Inc/global.h ../Core/Inc/main.h \
- ../Core/Inc/software_timer.h ../Core/Inc/software_timer.h \
- ../Core/Inc/fsm.h
+ ../Core/Inc/scheduler.h ../Core/Inc/software_timer.h ../Core/Inc/fsm.h \
+ ../Core/Inc/main.h ../Core/Inc/software_timer.h
 ../Core/Inc/main.h:
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h:
 ../Core/Inc/stm32f1xx_hal_conf.h:
@@ -58,8 +57,7 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h:
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_uart.h:
 ../Core/Inc/scheduler.h:
-../Core/Inc/global.h:
-../Core/Inc/main.h:
-../Core/Inc/software_timer.h:
 ../Core/Inc/software_timer.h:
 ../Core/Inc/fsm.h:
+../Core/Inc/main.h:
+../Core/Inc/software_timer.h:
