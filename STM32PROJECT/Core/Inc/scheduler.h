2da71ca142cadfb8b109bd09353f81a48f4c1851
yshic <tuanl799@gmail.com>
2023-12-09 09:38:25 +0700

    Revert "add scheduler"
    This reverts commit 2b34fafa5e6e938b97cb657885585b10e969c341.

--- STM32PROJECT/Core/Inc/scheduler.h
+++ STM32PROJECT/Core/Inc/scheduler.h
@@ -1,17 +1,15 @@
-#ifndef INC_SCHEDULER_H_
-#define INC_SCHEDULER_H_
+#ifndef APP_SCHEDULER_H_
+#define APP_SCHEDULER_H_
 
 #include "stdint.h"
 
-#define SCH_MAX_TASKS 	10
-#define	NO_TASK_ID		0
+
+#define SCH_MAX_TASKS 			10
 
 void SCH_Init(void);
 void SCH_Update(void);
+uint32_t SCH_Add_Task(void (*p_function)(), uint32_t DELAY, uint32_t PERIOD);
 void SCH_Dispatch_Tasks(void);
 uint8_t SCH_Delete_Task(uint32_t TASK_ID);
-uint32_t SCH_Add_Task(void (*p_function)(), uint32_t DELAY, uint32_t PERIOD);
 
-static uint32_t Get_New_Task_ID(void);
-
-#endif /* INC_SCHEDULER_H_ */
+#endif /* APP_SCHEDULER_H_ */
