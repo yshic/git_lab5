17a7fff96627cdef52d4a7ae3cb68070ea136d01
yshic <tuanl799@gmail.com>
2023-12-09 09:42:57 +0700

    build and generate hex file

--- STM32PROJECT/Debug/Core/Src/main.d
+++ STM32PROJECT/Debug/Core/Src/main.d
@@ -27,7 +27,7 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_uart.h \
  ../Core/Inc/scheduler.h ../Core/Inc/software_timer.h ../Core/Inc/fsm.h \
- ../Core/Inc/main.h ../Core/Inc/software_timer.h
+ ../Core/Inc/main.h
 ../Core/Inc/main.h:
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h:
 ../Core/Inc/stm32f1xx_hal_conf.h:
@@ -60,4 +60,3 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
 ../Core/Inc/software_timer.h:
 ../Core/Inc/fsm.h:
 ../Core/Inc/main.h:
-../Core/Inc/software_timer.h:
