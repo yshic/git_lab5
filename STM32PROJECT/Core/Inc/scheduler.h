2b34fafa5e6e938b97cb657885585b10e969c341
yshic <tuanl799@gmail.com>
2023-12-09 09:38:06 +0700

    add scheduler

--- STM32PROJECT/Core/Inc/scheduler.h
+++ STM32PROJECT/Core/Inc/scheduler.h
@@ -1,15 +1,17 @@
-#ifndef APP_SCHEDULER_H_
-#define APP_SCHEDULER_H_
+#ifndef INC_SCHEDULER_H_
+#define INC_SCHEDULER_H_
 
 #include "stdint.h"
 
-
-#define SCH_MAX_TASKS 			10
+#define SCH_MAX_TASKS 	10
+#define	NO_TASK_ID		0
 
 void SCH_Init(void);
 void SCH_Update(void);
-uint32_t SCH_Add_Task(void (*p_function)(), uint32_t DELAY, uint32_t PERIOD);
 void SCH_Dispatch_Tasks(void);
 uint8_t SCH_Delete_Task(uint32_t TASK_ID);
+uint32_t SCH_Add_Task(void (*p_function)(), uint32_t DELAY, uint32_t PERIOD);
 
-#endif /* APP_SCHEDULER_H_ */
+static uint32_t Get_New_Task_ID(void);
+
+#endif /* INC_SCHEDULER_H_ */
